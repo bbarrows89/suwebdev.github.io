9319aa27cac98bfd8b154b83ab1ac614d5d90596
Shawn Rider <shawn@shawnrider.com>
2014-07-16 10:44:42 -0700

    Added styles for wufoo contact form.
    Change-Id: I280664c05206760381219f6353ecf7c30810c35f

--- Gruntfile.js
+++ Gruntfile.js
@@ -311,6 +311,7 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
+                        '{,*/}*.css',
                         'sitemap.xml',
                         'CNAME',
                         'README*',
